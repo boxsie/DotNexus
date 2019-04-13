5849a8f98f20faf05d14dc29ac9a32fa0ce7d73e
danielsan <dan@boxsie.co.uk>
2019-04-13 23:15:58 +0100

    dockerfile update

--- DotNexus.App/Program.cs
+++ DotNexus.App/Program.cs
@@ -21,14 +21,7 @@ namespace DotNexus.App
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options =>
-                {
-                    options.Listen(IPAddress.Loopback, 5000);
-                    options.Listen(IPAddress.Loopback, 5001, listenOptions =>
-                    {
-                        listenOptions.UseHttps("cert.pfx", "topsecret");
-                    });
-                })
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
